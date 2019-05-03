8787e6035ca0003c5cf99770ca55d8aa41682e85
Gteoharov <georgia.teoharov@gmail.com>
2019-05-03 18:15:41 +0300

    Setup and test Firebase

--- Flash Chat/AppDelegate.swift
+++ Flash Chat/AppDelegate.swift
@@ -18,8 +18,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         //TODO: Initialise and Configure your Firebase here:
-        
         FirebaseApp.configure()
+        
+        let myDatabase = Database.database().reference()
+        
+        myDatabase.setValue("Test data!")
+        
         return true
     }
 
